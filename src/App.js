52b9fe289d194407f763c8b2813a913b5fea2d01
Arturo <arturoantoniotr@gmail.com>
2023-08-15 21:08:51 -0400

    Adding buttons and camera

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import logo from './plantsinwindow.gif';
+//import logo from './plantsinwindow.gif';
 import './App.css';
 
 function App() {
+  const callMotor1 = async () => {
+    try {
+      const response = await fetch('http://arturohouse.tplinkdns.com:8001/motor');
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const callMotor2 = async () => {
+    try {
+      const response = await fetch('http://arturohouse.tplinkdns.com:8001/motor2');
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  //const streamStyle = {display:"block", "-webkit-user-select": "none", "margin": "auto"}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="logo" />
+        <img src="http://arturohouse.tplinkdns.com:8002/stream" alt="logo"/>
         <p>
-          Proximamente, salvando a una planta de su madre ausente.
+          Salvando a una planta de su madre ausente.
         </p>
+        <button onClick={callMotor1}>Aguita pa Darky</button>
+        <button onClick={callMotor2}>Aguita pa Manolo</button>
       </header>
     </div>
   );
